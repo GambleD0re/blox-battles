cfffa9766e50ba3f12cf126c589b1f4a598eadaf
GambleDore <gambledorethewealthy@gmail.com>
2025-08-20 11:51:46 +0800

    Update index.js

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -78,33 +78,4 @@ const config = {
 };
 
 // Freeze the config object to prevent accidental mutations during runtime
-module.exports = Object.freeze(config);```
-
----
-**File 3 of 3**
-```javascript
-// /backend/src/config/database.js
-const { Pool } = require('pg');
-const config = require('./index');
-const logger = require('../utils/logger');
-
-const pool = new Pool({
-  connectionString: config.databaseUrl,
-  ssl: config.env === 'production' 
-    ? { rejectUnauthorized: false } 
-    : false,
-});
-
-pool.on('connect', () => {
-  logger.info('A client has connected to the database pool.');
-});
-
-pool.on('error', (err, client) => {
-  logger.error({ err, client }, 'Unexpected error on idle PostgreSQL client');
-  process.exit(-1);
-});
-
-module.exports = {
-  pool,
-  query: (text, params) => pool.query(text, params),
-};
+module.exports = Object.freeze(config);
