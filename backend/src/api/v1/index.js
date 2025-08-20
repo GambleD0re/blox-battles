5c0a26f27ef9564eec329e92ab81631cb31fd3d2
GambleDore <gambledorethewealthy@gmail.com>
2025-08-20 13:07:44 +0800

    Update index.js

--- backend/src/api/v1/index.js
+++ backend/src/api/v1/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const authRoutes = require('./routes/auth.routes');
 const userRoutes = require('./routes/user.routes');
 const gameRoutes = require('./routes/game.routes');
+const platformRoutes = require('./routes/platform.routes');
+const duelRoutes = require('./routes/duel.routes');
 
 const router = express.Router();
 
@@ -21,6 +23,8 @@ router.get('/', (req, res) => {
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/games', gameRoutes);
+router.use('/platforms', platformRoutes);
+router.use('/duels', duelRoutes);
 
 
 module.exports = router;
