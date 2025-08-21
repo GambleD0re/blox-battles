9f5e8c888da2b9fe1ddea6e22326c56e59d2f58c
GambleDore <gambledorethewealthy@gmail.com>
2025-08-21 10:29:44 +0800

    Create rivalsGameData.js

---
+++ backend/games/rivals/data/rivalsGameData.js
@@ -0,0 +1,37 @@
+// backend/games/rivals/data/rivalsGameData.js
+const GAME_DATA = {
+    regions: [
+        { id: 'NA-West', name: 'NA-West' },
+        { id: 'NA-East', name: 'NA-East' },
+        { id: 'EU', name: 'EU' },
+        { id: 'OCE', name: 'OCE' },
+    ],
+    maps: [
+        { id: 'arena', name: 'Arena' },
+        { id: 'backrooms', name: 'Backrooms' },
+        { id: 'battleground', name: 'Battleground' },
+        { id: 'crossroads', name: 'Crossroads' },
+        { id: 'docks', name: 'Docks' },
+        { id: 'graveyard', name: 'Graveyard' },
+        { id: 'onyx', name: 'Onyx' },
+        { id: 'playground', name: 'Playground' },
+        { id: 'splash', name: 'Splash' },
+        { id: 'station', name: 'Station' },
+    ],
+    weapons: [
+        { id: 'assault_rifle', name: 'Assault Rifle' },
+        { id: 'bow', name: 'Bow' },
+        { id: 'burst_rifle', name: 'Burst Rifle' },
+        { id: 'flamethrower', name: 'Flamethrower' },
+        { id: 'grenade_launcher', name: 'Grenade Launcher' },
+        { id: 'minigun', name: 'Minigun' },
+        { id: 'paintball_gun', name: 'Paintball Gun' },
+        { id: 'rpg', name: 'RPG' },
+        { id: 'shotgun', 'name': 'Shotgun' },
+        { id: 'sniper_rifle', name: 'Sniper Rifle' },
+        { id: 'exogun', name: 'Exogun' },
+        { id: 'flare_gun', name: 'Flare Gun' },
+    ]
+};
+
+module.exports = GAME_DATA;
