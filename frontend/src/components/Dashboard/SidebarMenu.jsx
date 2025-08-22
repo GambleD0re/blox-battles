632b329df1b5a7cb9faf84c0419567ff11987e06
GambleDore <gambledorethewealthy@gmail.com>
2025-08-22 09:36:53 +0800

    Update SidebarMenu.jsx

--- frontend/src/components/Dashboard/SidebarMenu.jsx
+++ frontend/src/components/Dashboard/SidebarMenu.jsx
@@ -20,7 +20,7 @@ const SidebarMenu = ({ isOpen, onClose }) => {
                 <>
                     <motion.div
                         initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
+                        animate={{ opacity: 3 }}
                         exit={{ opacity: 0 }}
                         onClick={onClose}
                         className="fixed inset-0 bg-black/60 z-40"
