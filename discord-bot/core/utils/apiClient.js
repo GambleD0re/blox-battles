94265b265e5601f39703ba053ad22337752f360e
GambleDore <gambledorethewealthy@gmail.com>
2025-08-22 14:08:15 +0800

    Create apiClient.js

---
+++ discord-bot/core/utils/apiClient.js
@@ -0,0 +1,19 @@
+// discord-bot/core/utils/apiClient.js
+const axios = require('axios');
+
+const { BOT_API_KEY, BACKEND_API_URL } = process.env;
+
+if (!BOT_API_KEY || !BACKEND_API_URL) {
+    console.error("FATAL: BOT_API_KEY or BACKEND_API_URL is not defined for the bot's apiClient.");
+    process.exit(1);
+}
+
+const apiClient = axios.create({
+    baseURL: `${BACKEND_API_URL}/api`,
+    headers: {
+        'Content-Type': 'application/json',
+        'X-API-Key': BOT_API_KEY
+    }
+});
+
+module.exports = { apiClient };
