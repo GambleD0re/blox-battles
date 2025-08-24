dc332ebdab2490fecb7d1e7c6abb3285cb20503f
GambleDore <gambledorethewealthy@gmail.com>
2025-08-24 16:05:52 +0800

    Update ready.js

--- discord-bot/core/events/ready.js
+++ discord-bot/core/events/ready.js
@@ -1,7 +1,7 @@
 // discord-bot/core/events/ready.js
 const { Events } = require('discord.js');
 const { apiClient } = require('../utils/apiClient');
-const { cacheGameData } = require('../utils/gameData');
+const { cacheGameData, cacheActiveGamesList } = require('../utils/gameData');
 const { startTaskProcessor } = require('../tasks/taskProcessor');
 const { startStatusUpdaters } = require('../utils/statusUpdater');
 
@@ -11,10 +11,15 @@ module.exports = {
     async execute(client) {
         console.log(`Bot logged in as ${client.user.tag}!`);
         try {
+            // [MODIFIED] Fetch the list of games first
+            await cacheActiveGamesList(); 
             const { data: activeGames } = await apiClient.get('/games');
+            
+            // Then cache the specific data for each game
             for (const game of activeGames) {
                 await cacheGameData(game.id);
             }
+
             startTaskProcessor(client);
             startStatusUpdaters(client);
             console.log("Bot initialization complete. Systems are running.");
