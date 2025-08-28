cd240d6164b8d07f1eb2f1ce0bc4c1c1986dd035
GambleDore <gambledorethewealthy@gmail.com>
2025-08-28 11:30:49 +0800

    Update MainLayout.jsx

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
     return (
-        <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 pb-28">
+        <div className="max-w-screen-2xl mx-auto p-4 sm:p-6 lg:p-8 pb-24">
             <Outlet />
         </div>
     );
